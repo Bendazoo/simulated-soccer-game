29aa42b949373007040230802b59a46a341acbe8
bendazoo <abendezu385@gmail.com>
2024-01-30 17:09:02 -0500

    SoccerTeam class added

---
+++ simulatedSoccerGame/src/simulatedSoccerGame/SoccerTeam.java
@@ -0,0 +1,18 @@
+package simulatedSoccerGame;
+
+public class SoccerTeam {
+	
+	
+
+	String teamname;
+
+	SoccerTeam(String teamname){
+		
+		this.teamname = teamname;
+		
+		
+	}
+
+	
+
+}
--- simulatedSoccerGame/src/simulatedSoccerGame/main.java
+++ simulatedSoccerGame/src/simulatedSoccerGame/main.java
@@ -1,24 +1,30 @@
 package simulatedSoccerGame;
-import java.util.*;
+		import java.util.*;
 
-public class main {
-	
-	public static void main(String[] args) {
-		
-		Random random = new Random();
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("5v5 Simulated Game");
-		System.out.println("Rules: ");
-		System.out.println("There are a total of 30 turns, in which there will be a roll of a six sided dice.");
-		System.out.println("The number rolled will determine what is the action fo your team.");
-		
-		
-		
-		
-		
-		
-		
-	}
-
-}
+		public class main {
+			
+			public static void main(String[] args) {
+				
+				Random random = new Random();
+				Scanner scanner = new Scanner(System.in);
+				
+				System.out.println("5v5 Simulated Game");
+				System.out.println("Rules: ");
+				System.out.println("There are a total of 30 turns, in which there will be a roll of a six sided dice.");
+				System.out.println("The number rolled will determine what is the action fo your team.\n");
+				
+				
+				System.out.println("What do you want to name your name player One? ");
+				String playerOneTeam = scanner.nextLine();
+				
+				System.out.println("What do you want to name your name player Two? ");
+				String playerTwoTeam = scanner.nextLine();
+				
+				SoccerTeam soccerteamOne = new SoccerTeam(playerOneTeam); 
+				SoccerTeam soccerteamTwo = new SoccerTeam(playerTwoTeam); 
+				
+				System.out.printf("\nWelcome %s and %s to the Goochland Grand Finals, the winnder of this game will win the cup!", soccerteamOne.teamname, soccerteamTwo.teamname);
+				
+				
+			}
+		}
