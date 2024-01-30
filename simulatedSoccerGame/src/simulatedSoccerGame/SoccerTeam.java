6d6002c6eef46f9642a4d64f8381522b2ba602ff
bendazoo <abendezu385@gmail.com>
2024-01-30 17:29:19 -0500

    Added cpu teams for each team

--- simulatedSoccerGame/src/simulatedSoccerGame/SoccerTeam.java
+++ simulatedSoccerGame/src/simulatedSoccerGame/SoccerTeam.java
@@ -1,16 +1,39 @@
 package simulatedSoccerGame;
+import java.util.ArrayList;
 
 public class SoccerTeam {
 	
 	
 
 	String teamname;
+	static int check = 0;
+	ArrayList <String> cputeam = new ArrayList();
+	
 
 	SoccerTeam(String teamname){
 		
+		
+		
 		this.teamname = teamname;
+		if (check == 0 ) {
+			this.cputeam.add("R1");
+			this.cputeam.add("R2");
+			this.cputeam.add("R3");
+			this.cputeam.add("R4");
+			this.cputeam.add("R5");
+			
+			check++;
+		} else {
+			this.cputeam.add("B1");
+			this.cputeam.add("B2");
+			this.cputeam.add("B3");
+			this.cputeam.add("B4");
+			this.cputeam.add("B5");
+			
+		}
 		
 		
+	
 	}
 
 	
--- simulatedSoccerGame/src/simulatedSoccerGame/main.java
+++ simulatedSoccerGame/src/simulatedSoccerGame/main.java
@@ -1,29 +1,35 @@
 package simulatedSoccerGame;
-		import java.util.*;
+import java.util.*;
 
-		public class main {
+public class main {
 			
-			public static void main(String[] args) {
+	public static void main(String[] args) {
 				
-				Random random = new Random();
-				Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		Scanner scanner = new Scanner(System.in);
 				
-				System.out.println("5v5 Simulated Game");
-				System.out.println("Rules: ");
-				System.out.println("There are a total of 30 turns, in which there will be a roll of a six sided dice.");
-				System.out.println("The number rolled will determine what is the action fo your team.\n");
+		System.out.println("5v5 Simulated Game");
+		System.out.println("Rules: ");
+		System.out.println("There are a total of 30 turns, in which there will be a roll of a six sided dice.");
+		System.out.println("The number rolled will determine what is the action for your team.\n");
 				
 				
-				System.out.println("What do you want to name your name player One? ");
-				String playerOneTeam = scanner.nextLine();
+		System.out.println("What do you want to name your team, player One? ");
+		String playerOneTeam = scanner.nextLine();
+		SoccerTeam soccerteamOne = new SoccerTeam(playerOneTeam); 
 				
-				System.out.println("What do you want to name your name player Two? ");
-				String playerTwoTeam = scanner.nextLine();
+		System.out.println("What do you want to name your team, player Two? ");
+		String playerTwoTeam = scanner.nextLine(); 
+		SoccerTeam soccerteamTwo = new SoccerTeam(playerTwoTeam); 
 				
-				SoccerTeam soccerteamOne = new SoccerTeam(playerOneTeam); 
-				SoccerTeam soccerteamTwo = new SoccerTeam(playerTwoTeam); 
 				
-				System.out.printf("\nWelcome %s and %s to the Goochland Grand Finals, the winnder of this game will win the cup!", soccerteamOne.teamname, soccerteamTwo.teamname);
+		System.out.printf("\nWelcome %s (R team) and %s (B team) to the Goochland Grand Finals, the winner of this game will win the cup!", soccerteamOne.teamname, soccerteamTwo.teamname);
+			
+		
+		System.out.println(soccerteamOne.cputeam);
+		System.out.println(soccerteamTwo.cputeam);
+				
+		
 				
 				
 			}
