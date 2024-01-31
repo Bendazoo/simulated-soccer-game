94042a3019dccfdc7ccef69c016f8ecd3e3b6e21
bendazoo <abendezu385@gmail.com>
2024-01-30 20:36:30 -0500

    added methods for actions in football game

--- simulatedSoccerGame/src/simulatedSoccerGame/SoccerTeam.java
+++ simulatedSoccerGame/src/simulatedSoccerGame/SoccerTeam.java
@@ -1,20 +1,26 @@
 package simulatedSoccerGame;
-import java.util.ArrayList;
+import java.util.*;
+
+
 
 public class SoccerTeam {
-	
-	
 
 	String teamname;
 	static int check = 0;
 	ArrayList <String> cputeam = new ArrayList();
-	
+	Random random = new Random();
+	Scanner scanner = new Scanner(System.in);
+	int goals = 0;
+	int userRollMain = random.nextInt(6) +1;
+	int userRollOpp = random.nextInt(6) +1;
+	int randomPlayer = random.nextInt(4);
 
 	SoccerTeam(String teamname){
 		
-		
-		
 		this.teamname = teamname;
+		this.goals = goals;
+		
+		
 		if (check == 0 ) {
 			this.cputeam.add("R1");
 			this.cputeam.add("R2");
@@ -30,12 +36,71 @@ public class SoccerTeam {
 			this.cputeam.add("B4");
 			this.cputeam.add("B5");
 			
-		}
-		
+		}	
 		
 	
 	}
-
 	
-
+	void chance_goal() {
+		userRollMain = random.nextInt(6) +1;
+		userRollOpp = random.nextInt(6) +1;
+		
+		if(userRollMain > userRollOpp) {
+			this.goals ++;
+			System.out.println(this.teamname + " have scored!\n");
+			
+			
+		} else {
+			System.out.println(this.teamname + " have failed to secure the goal!\n");
+		}
+		
+	}
+	void chance_foul() {
+		userRollMain = random.nextInt(6) +1;
+		userRollOpp = random.nextInt(6) +1;
+		
+		if (userRollMain >=2) {
+			System.out.println("Looks like the foul was clean and no card will be given\n");
+		} else if(userRollMain == 1) {
+			int playerRandom = random.nextInt(4);
+			System.out.println("A bad foul from " + this.cputeam.get(playerRandom) + " has offered an opportunity for the opposing team to score a penalty!\n" );
+			this.cputeam.remove(playerRandom);
+			chance_penalty();
+			
+		}
+		
+	}
+	void chance_injured() {
+		userRollMain = random.nextInt(6) +1;
+		userRollOpp = random.nextInt(6) +1;
+		
+	}
+	void chance_penalty() {
+		userRollMain = random.nextInt(6) +1;
+		userRollOpp = random.nextInt(6) +1;
+		
+	}
+	void chance_freekick() {
+		userRollMain = random.nextInt(6) +1;
+		userRollOpp = random.nextInt(6) +1;
+		int playerRandom = random.nextInt(4);
+		
+		if(userRollMain >userRollOpp) {
+			
+			System.out.println(this.cputeam.get(playerRandom) + " has scored the freekick!\n");
+			this.goals ++;
+		} else {
+			System.out.println(this.cputeam.get(playerRandom) + " has missed the freekick!\n");
+		}
+		
+	}
+	void chance_sleep() {
+		return;
+	}
+	
+	void injured() {
+			int playerRandom = random.nextInt(4);
+			System.out.println("The player " + this.cputeam.get(playerRandom) + " has gotten injured and no card was given!\n" );
+			
+	}
 }
--- simulatedSoccerGame/src/simulatedSoccerGame/main.java
+++ simulatedSoccerGame/src/simulatedSoccerGame/main.java
@@ -27,38 +27,79 @@ public class main {
 				
 		System.out.printf("\nWelcome %s (R team) and %s (B team) to the Goochland Grand Finals, the winner of this game will win the cup!", soccerteamOne.teamname, soccerteamTwo.teamname);
 			
-		
-		
-		int i = 3;
+		int matchtime = 3;
 		int time = 3;
-		
 		System.out.println("\nThe game starts in...");
 		
 		do {
 			System.out.println(time +  "!" );
 			time = time - 1;
-			i = i-1;
 			try {
-				
 				TimeUnit.MILLISECONDS.sleep(1000);
 				
 			} catch(InterruptedException ex) {
 			}
-		} while(i>=1);
+		} while(time>=1);
 		
 		System.out.println("The match begins!");
+		
+		
+		do {
+			try {
+				TimeUnit.MILLISECONDS.sleep(1000);
+				
+			} catch(InterruptedException ex) {
+			}
+			if(matchtime == 3) {
+				System.out.println("\n(3\") It is the 3rd minute of the game.");
+			} else {
+				System.out.println("\n("+ matchtime + "\")"+ "It is the " + matchtime + "th minute of the game.");
+			}
+			matchtime += 3;	
+			
+			int situationRoll = random.nextInt(6) + 1;
+			
+			if((matchtime%2) != 0) {
+				switch(situationRoll) {
+				case 1: soccerteamOne.chance_goal();
+					break;
+				case 2: soccerteamOne.chance_foul();
+					break;
+				case 3: soccerteamOne.chance_freekick();
+					break;
+				case 4: if(soccerteamOne.userRollMain == 6) {
+					soccerteamTwo.injured();
+				} else {
+					soccerteamOne.chance_foul();
+				}
+					break;
+				case 5: soccerteamOne.chance_penalty();
+					break;
+				case 6: soccerteamOne.chance_sleep();
+					break;
+				}
+				
+			} else {
+				switch(situationRoll) {
+				case 1: soccerteamTwo.chance_goal();
+					break;
+				case 2: soccerteamTwo.chance_foul();
+					break;
+				case 3: soccerteamTwo.chance_freekick();
+					break;
+				case 4: soccerteamTwo.chance_injured();
+					break;
+				case 5: soccerteamTwo.chance_penalty();
+					break;
+				case 6: soccerteamTwo.chance_sleep();
+					break;
+				}
+			}
+		} while(matchtime<=90);
+				
 			
 		
 		
-
-			
-			
-			
-			
-
-				
-		
-				
-				
+		System.out.printf("FINAL SCORE: %s %d -  %d %s :",soccerteamOne.teamname , soccerteamOne.goals, soccerteamTwo.goals,soccerteamTwo.teamname );
 			}
 		}
